581c61a085857fab970ea0754de1fd66f851a6ee
SlaxXxX <Janis.Schneider97@web.de>
2018-11-01 14:12:49 +0100

    Back to clean start

--- android/src/de/codecrunch/AndroidLauncher.java
+++ android/src/de/codecrunch/AndroidLauncher.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.badlogic.gdx.backends.android.surfaceview.RatioResolutionStrategy;
-import de.codecrunch.BdxApp;
+import de.codecrunch.TowerAttackGame;
 
 public class AndroidLauncher extends AndroidApplication {
-
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		int width = 800;
-		int height = 450;
-		config.resolutionStrategy = new RatioResolutionStrategy(width, height);
-		initialize(new BdxApp(), config);
+		initialize(new TowerAttackGame(), config);
 	}
 }
