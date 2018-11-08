15538065bf2c70b270d2577a0b5e48085ce1fe42
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2018-11-08 16:40:39 +0100

    ADDED Cucumber, Espresso & Green Coffee
    added integration of Cucumber, espresso and tried to integrate Green Coffee but can not resolve com.mauriciotogneri:greencoffee:3.5.0

---
+++ android/src/de/codecrunch/test/de/codecrunch/test/SelectLevelSteps.java
@@ -0,0 +1,110 @@
+package de.codecrunch.test;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+class SelectLevelSteps {
+    @When("^I click on the level I want to play$")
+    public void iClickOnTheLevelIWantToPlay() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I solved the previous level or it is the first level$")
+    public void iSolvedThePreviousLevelOrItIsTheFirstLevel() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I the selected level should start$")
+    public void iTheSelectedLevelShouldStart() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I did not solve the previous level$")
+    public void iDidNotSolveThePreviousLevel() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should see the 'level needs to be unlocked' message$")
+    public void iShouldSeeTheLevelNeedsToBeUnlockedMessage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I stay at the 'select level' screen$")
+    public void iStayAtTheSelectLevelScreen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I am not at the top of the List$")
+    public void iAmNotAtTheTopOfTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on the upper arrow$")
+    public void iClickOnTheUpperArrow() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I stay on the 'select level' screen$")
+    public void iStayOnTheSelectLevelScreen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I get to see levels wih a lower number$")
+    public void iGetToSeeLevelsWihALowerNumber() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I am at the top of the List$")
+    public void iAmAtTheTopOfTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I get to see the 'select level' screen$")
+    public void iGetToSeeTheSelectLevelScreen() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I get to see the same levels$")
+    public void iGetToSeeTheSameLevels() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I am not at the bottom of the List$")
+    public void iAmNotAtTheBottomOfTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I click on the lower arrow$")
+    public void iClickOnTheLowerArrow() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I get to see levels wih a higher number$")
+    public void iGetToSeeLevelsWihAHigherNumber() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I am at the bottom of the List$")
+    public void iAmAtTheBottomOfTheList() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
