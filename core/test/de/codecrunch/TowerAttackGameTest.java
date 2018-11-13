48881800e5c9b3ecb1049b83bcbf6f4e66e1dcba
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2018-11-13 16:59:44 +0100

    ADDED structure to build unit and integrity tests in
    apperently testing with LibGDX in AndroidStudio is a little bit different than just testing an AndroidApp
    I've found this:
    http://manabreak.eu/java/2016/10/21/unittesting-libgdx.html
    And I tried to implement  it in out project

---
+++ core/test/de/codecrunch/TowerAttackGameTest.java
@@ -0,0 +1,30 @@
+package de.codecrunch;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TowerAttackGameTest extends GameTest{
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void render() {
+    }
+
+    @Test
+    public void dispose() {
+    }
+}
