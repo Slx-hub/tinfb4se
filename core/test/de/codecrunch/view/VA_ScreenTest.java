8c0bd5d0f7b73a0a4db58b890a14385abb708dae
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2018-11-21 13:04:37 +0100

    little cleanup after merge

---
+++ core/test/de/codecrunch/view/VA_ScreenTest.java
@@ -0,0 +1,36 @@
+package de.codecrunch.view;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VA_ScreenTest {
+
+    @Test
+    public void show() {
+    }
+
+    @Test
+    public void hide() {
+    }
+
+    @Test
+    public void render() {
+    }
+
+    @Test
+    public void resize() {
+    }
+
+    @Test
+    public void pause() {
+    }
+
+    @Test
+    public void resume() {
+    }
+
+    @Test
+    public void dispose() {
+    }
+}
--- core/test/de/codecrunch/view/screen/VA_ScreenTest.java
+++
@@ -1,7 +0,0 @@
-package de.codecrunch.view.screen;
-
-import static org.junit.Assert.*;
-
-public class VA_ScreenTest {
-
-}
