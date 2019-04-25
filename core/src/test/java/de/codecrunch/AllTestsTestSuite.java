25cb4d180558c860737026f8c3730f23a3cd853a
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-04-25 12:09:36 +0200

    added UnitTests for Towers and already fixed found errors

--- core/src/test/java/de/codecrunch/AllTestsTestSuite.java
+++ core/src/test/java/de/codecrunch/AllTestsTestSuite.java
@@ -3,9 +3,13 @@ package de.codecrunch;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import de.codecrunch.model.tower.AllTowerTestSuite;
 import de.codecrunch.model.unit.AllUnitsTestSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AllUnitsTestSuite.class})
+@Suite.SuiteClasses({
+        AllUnitsTestSuite.class,
+        AllTowerTestSuite.class
+})
 public class AllTestsTestSuite {
 }
