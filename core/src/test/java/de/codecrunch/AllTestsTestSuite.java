9dfd0095a0ead7133bf1fdbb7671afb676a2a6fe
tomorow94 <40265192+tomorow94@users.noreply.github.com>
2019-04-25 09:36:31 +0200

    Create AllTestsTestSuite.java

---
+++ core/src/test/java/de/codecrunch/AllTestsTestSuite.java
@@ -0,0 +1,11 @@
+package de.codecrunch;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import de.codecrunch.model.unit.AllUnitsTestSuite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AllUnitsTestSuite.class})
+public class AllTestsTestSuite {
+}
